56265cb6efc826133b9030d6e3f35bbd425edee5
Hassan-Maide-Gaga <hassanmaidegaga@gmail.com>
2024-12-07 03:22:34 +0000

    Cours 1

--- gestion_dette/gestion.php
+++ gestion_dette/gestion.php
@@ -1,4 +1,8 @@
 <?php 
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
 // Fonctions d'accès aux données
 function selectClients():array {
     return [
